4fd0149196d0ffde9e01ad11425d36af925531c6
devAdityaa <acolydebaian8@gmail.com>
2024-02-09 12:44:30 +0530

    Final prototype 1

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,8 @@
 chrome.webNavigation.onCompleted.addListener((details)=>{
     if(details.url){
         if(details.url.includes("amazon.")){
-            chrome.tabs.sendMessage(details.tabId,{
+console.log("Start")
+                        chrome.tabs.sendMessage(details.tabId,{
                 type:'start',
                 tabid:details.tabId,
                 message:"amazon"
@@ -10,7 +11,7 @@ chrome.webNavigation.onCompleted.addListener((details)=>{
     }
 })
 
-chrome.runtime.onMessage.addListener((req,sen,res)=>{
+chrome.runtime.onMessage.addListener(async (req,sen,res)=>{
     console.log("Received!",req)
 if(req.type==='translate'){
     const headers = {
@@ -25,7 +26,7 @@ if(req.type==='translate'){
                 "taskType": "translation",
                 "config": {
                     "language": {
-                        "sourceLanguage": "bn",
+                        "sourceLanguage": "hi",
                         "targetLanguage": "en"
                     },
                     "serviceId": "ai4bharat/indictrans-v2-all-gpu--t4"
@@ -51,6 +52,55 @@ if(req.type==='translate'){
         console.log(sen)
         chrome.tabs.sendMessage(sen.tab.id,{type:'translated',translated:translated})
     })
+}
+else if(req.type==='translate-items'){
+    console.log("Trigger 2")
+    console.log("Received items")
+    const headers = {
+        'Content-Type': 'application/json',
+        "Authorization":"jJMLg0c0QjUiXHb_D7DjRVU1vV9FEPc72XeaAeZ5G1FSwGXFJQPCm9oytt4orGkW"
+    }
+    const url = "https://dhruva-api.bhashini.gov.in/services/inference/pipeline"
+    const elements = JSON.parse(req.value);
+    console.log("Items array:", elements)
+    let e=[]
+    for(let i=0;i<elements.length;i++){
+        
+        const body2 = {
+            "pipelineTasks": [
+                {
+                    "taskType": "translation",
+                    "config": {
+                        "language": {
+                            "sourceLanguage": "en",
+                            "targetLanguage": "hi"
+                        },
+                        "serviceId": "ai4bharat/indictrans-v2-all-gpu--t4"
+                    }
+                }
+            ],
+            "inputData": {
+                "input": [
+                    {
+                        "source": `${elements[i]}`
+                    }
+                ],
+                "audio": [
+                    {
+                        "audioContent": null
+                    }
+                ]
+            }
+        }
+        let data = await fetch(url,{ method: 'POST', headers: headers, body: JSON.stringify(body2) }).then(ob => ob.json()).then(data => {
+            const translated = data['pipelineResponse'][0]['output'][0]['target'];
+            return translated
+        })
+        console.log(data)
+        e.push(data)
+    }
+    chrome.tabs.sendMessage(sen.tab.id, {type:'translated-items', translated:JSON.stringify(e)})
+
 }
 })
 
--- scripts/injected.js
+++ scripts/injected.js
@@ -1,47 +1,84 @@
+const translateSearchResultsText =()=>{
+    console.log("Trigger 1")
+    const orientationType1 = document.querySelectorAll('div > div > div > div > span > div > div > div.a-section.a-spacing-small.puis-padding-left-small.puis-padding-right-small > div.a-section.a-spacing-none.a-spacing-top-small.s-title-instructions-style > h2');
+    const orientationType2 = document.querySelectorAll('div > div > span > div > div > div > div.puisg-col.puisg-col-4-of-12.puisg-col-8-of-16.puisg-col-12-of-20.puisg-col-12-of-24.puis-list-col-right > div > div > div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style > h2');
+
+    const htmlElements = orientationType1.length!=0? orientationType1:orientationType2;
+console.log(htmlElements,orientationType1,orientationType2)
+        const textElements = Array.from(htmlElements, element => element.innerText)
+    let e = chrome.runtime.sendMessage({type:'translate-items', value:JSON.stringify(textElements)})
+}
+
+const insertTranslatedText = (textElements)=>{
+    const orientationType1 = document.querySelectorAll('div > div > div > div > span > div > div > div.a-section.a-spacing-small.puis-padding-left-small.puis-padding-right-small > div.a-section.a-spacing-none.a-spacing-top-small.s-title-instructions-style > h2');
+    const orientationType2 = document.querySelectorAll('div > div > span > div > div > div > div.puisg-col.puisg-col-4-of-12.puisg-col-8-of-16.puisg-col-12-of-20.puisg-col-12-of-24.puis-list-col-right > div > div > div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style > h2');
+    const htmlElements = orientationType1.length!=0? orientationType1:orientationType2;
+    htmlElements.forEach((element,index)=>{
+        element.innerText = textElements[index];
+    })
+
+}
+
+
+
 (()=>{
-    chrome.runtime.onMessage.addListener((req,res)=>{
+window.addEventListener('load',()=>{
+        chrome.runtime.onMessage.addListener((req,res)=>{
 
-        console.log("Injected!")
-        if(req.type==='start'){
-        const submit = document.querySelector('#nav-search-bar-form > div.nav-right > div');
-        submit.setAttribute("style","display:none")
-        if(!(document.getElementById('bhasa'))){
-            const newSubmit = document.createElement('button');
-            newSubmit.innerText = "Search";
-            newSubmit.id = "bhasa";
-            document.querySelector('#nav-search').appendChild(newSubmit);
-        }
-
-        const btn = document.getElementById('bhasa');
-       
-        btn.addEventListener('click',()=>{
-            const form = document.querySelector('#twotabsearchtextbox');
-            let val = form.value;
-            chrome.runtime.sendMessage({type:'translate',value:val,id:req.tabId});
-            
-        })
+    console.log("Injected!")
+    if(req.type==='start'){
+    const submit = document.querySelector('#nav-search-bar-form > div.nav-right > div');
+    submit.setAttribute("style","display:none")
+    if(!(document.getElementById('bhasa'))){
+        const newSubmit = document.createElement('button');
+        newSubmit.innerText = "Search";
+        newSubmit.id = "bhasa";
+        document.querySelector('#nav-search').appendChild(newSubmit);
     }
-    else if(req.type==='translated'){
-        console.log("Res",req)
+
+    const btn = document.getElementById('bhasa');
+   
+    btn.addEventListener('click',()=>{
         const form = document.querySelector('#twotabsearchtextbox');
-        form.value = req.translated;
-        const actualBtn = document.querySelector('#nav-search-submit-button');
-        actualBtn.click();
-        const res = document.querySelector('#search');
-        res.style.opacity = 0.2;
+        let val = form.value;
+        chrome.runtime.sendMessage({type:'translate',value:val,id:req.tabId});
         
-    }
+    })
+}
+else if(req.type==='translated'){
+    console.log("Res",req)
+    const form = document.querySelector('#twotabsearchtextbox');
+    form.value = req.translated;
+    const actualBtn = document.querySelector('#nav-search-submit-button');
+    actualBtn.click();
+    const res = document.querySelector('#search');
+    res.style.opacity = 0.2;
+    
+}
 
-    else if(req.type==='search'){
-        const res = document.querySelector('#search');
-        res.style.opacity = 0.2;
-        const img = document.createElement('img');
-        console.log("URL:")
-        console.log(chrome.runtime.getURL('loader.gif'))
-        img.src = chrome.runtime.getURL('./assets/loader.gif');
-        img.id = 'loader';
-        res.appendChild(img);
-    }
+else if(req.type==='search'){
+    const res = document.querySelector('#search');
+    res.style.opacity = 0.2;
+    const img = document.createElement('img');
+    console.log("URL:")
+    console.log(chrome.runtime.getURL('loader.gif'))
+    img.src = chrome.runtime.getURL('./assets/loader.gif');
+    img.id = 'loader';
+    res.appendChild(img);
+    console.log("Trigger 0")
+    translateSearchResultsText()
+}
+else if(req.type==='translated-items'){
+    console.log("Trigger 4")
+    const a = JSON.parse(req.translated)
+    console.log(a)
+    insertTranslatedText(a);
+    const loader = document.getElementById('loader');
+    const res = document.querySelector('#search');
+    res.style.opacity = 1;
+    loader.remove();
+}
+})
 
     })
-})()
+        })()
